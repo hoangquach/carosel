b498c709af80da3179244c03b8a4263db541782a
hoangquach <78162869+hoangquach@users.noreply.github.com>
2024-08-10 16:42:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,12 +9,12 @@
 var radius = 240; // how big of the radius
 var autoRotate = true; // auto rotate or not
 var rotateSpeed = -60; // unit: seconds/360 degrees
-var imgWidth = 120; // width of images (unit: px)
-var imgHeight = 170; // height of images (unit: px)
+var imgWidth = 150; // width of images (unit: px) 120
+var imgHeight = 212.50; // height of images (unit: px) 170
 
 // Link of background music - set 'null' if you dont want to play background music
-var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
-var bgMusicControls = true; // Show UI music control
+//var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
+var bgMusicControls = false; // Show UI music control
 
 /*
      NOTE:
@@ -26,8 +26,8 @@ var bgMusicControls = true; // Show UI music control
 
 
 // ===================== start =======================
-// animation start after 1000 miliseconds
-setTimeout(init, 1000);
+// animation start after 1500 miliseconds
+setTimeout(init, 1500);
 
 var odrag = document.getElementById('drag-container');
 var ospin = document.getElementById('spin-container');
