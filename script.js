34ba1cc296906de093fa6d86f523617590068c70
hoangquach <78162869+hoangquach@users.noreply.github.com>
2024-08-15 23:41:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ var imgWidth = 200; // width of images (unit: px)
 var imgHeight = 270; // height of images (unit: px)
 
 // Link of background music - set 'null' if you dont want to play background music
-// var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
-// var bgMusicControls = true; // Show UI music control
+var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
+var bgMusicControls = true; // Show UI music control
 
 /*
      NOTE:
